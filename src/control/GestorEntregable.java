06ed3e73d2a4ee8e2c0ba5d2217ed8a733ea3e83
BrayanMQ <brayanmq11288@gmail.com>
2019-10-29 19:22:39 -0600

    buscarEtregable

--- src/control/GestorEntregable.java
+++ src/control/GestorEntregable.java
@@ -1,10 +1,30 @@
 
 package control;
 
+import modelo.Casillero;
+import modelo.Entregable;
+import modelo.Paquete;
+import modelo.TPaquete;
+
 public class GestorEntregable {
     
-    public void crearPaquete(){
-    
+    public Paquete crearPaquete(int id, double peso, String pDescripcion, 
+            String pRemitente, String pTipoPaquete, 
+            boolean pFragil, boolean pContenidoElectronico){
+        
+        TPaquete Tpaquete;
+        
+        if (TPaquete.Bolsa.toString().equals(pTipoPaquete)) {
+            Tpaquete = TPaquete.Bolsa;
+        } else {
+            Tpaquete = TPaquete.Caja;
+        }
+        
+        Paquete paquete = new Paquete(Tpaquete, pFragil, 
+                pContenidoElectronico, id, peso, pDescripcion, pRemitente);
+        
+        return paquete;
+
     }
     
     public void crearSobre(){
@@ -13,6 +33,18 @@ public class GestorEntregable {
     
     public void crearRevista(){
     
+    }
+    
+    public boolean buscarEntregableEInsertar(Entregable pEntregable, Casillero casillero){
+        int index = casillero.getListaEntregables().indexOf((pEntregable));
+        
+        if (index == -1) {
+            casillero.getListaEntregables().add(pEntregable);
+            return true;
+        }
+        
+        return false;
+        
     }
     
 }
